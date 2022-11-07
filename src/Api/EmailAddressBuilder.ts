1edad4de3ec501cb65bf83bd92ee47b0b8bb07ec
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2022-11-07 21:53:41 +0100

    fix: move augmented type into peer dependency (#47)
    BREAKING_CHANGE: upgrade to email-templates@^10.x.x, move augmented type into peer dependency

---
+++ src/Api/EmailAddressBuilder.ts
@@ -0,0 +1,39 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Date: 2022-11-07T21:11:64+01:00
+ * @Copyright: Technology Studio
+**/
+
+import type { Address } from 'nodemailer/lib/mailer'
+
+export class EmailAddressBuilder {
+  list: (string | Address)[]
+
+  static normalise = (value: string | Address | (string | Address)[] | undefined): (string | Address)[] => (
+    value ? typeof value === 'object' && Array.isArray(value) ? value : [value] : []
+  )
+
+  static equals = (left: string | Address, right: string | Address): boolean => (
+    (typeof left === 'string' ? left : left.address) === (typeof right === 'string' ? right : right.address)
+  )
+
+  constructor (initial: string | Address | (string | Address)[] | undefined) {
+    this.list = EmailAddressBuilder.normalise(initial)
+  }
+
+  add (value: string | Address | (string | Address)[] | undefined): EmailAddressBuilder {
+    const normalisedValue = EmailAddressBuilder.normalise(value)
+
+    normalisedValue.forEach(newEmailAddress => {
+      if (!this.list.find(existingEmailAddress => EmailAddressBuilder.equals(existingEmailAddress, newEmailAddress))) {
+        this.list.push(newEmailAddress)
+      }
+    })
+
+    return this
+  }
+
+  get (): string | Address | (string | Address)[] | undefined {
+    return this.list.length > 1 ? this.list : this.list.length === 1 ? this.list[0] : undefined
+  }
+}
--- src/Services/Types/EmailServiceTypes.ts
+++ src/Services/Types/EmailServiceTypes.ts
@@ -5,17 +5,9 @@
  * @Copyright: Technology Studio
 **/
 
-import type { EmailOptions } from 'email-templates'
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface AllEmailTemplates {
-
-}
-
-type EmailTemplate = keyof AllEmailTemplates
-
-type GetEmailTemplateOptions<EMAIL_TEMPLATE extends EmailTemplate> = AllEmailTemplates[EMAIL_TEMPLATE] extends EmailOptions ? AllEmailTemplates[EMAIL_TEMPLATE] : never
-
+import type {
+  EmailTemplate, GetEmailTemplateOptions,
+} from '@txo-peer-dep/email-service-node'
 export type Service = {
   sendEmail: <EMAIL_TEMPLATE extends EmailTemplate>(template: EMAIL_TEMPLATE, emailOptions: GetEmailTemplateOptions<EMAIL_TEMPLATE>) => Promise<void>,
 }
